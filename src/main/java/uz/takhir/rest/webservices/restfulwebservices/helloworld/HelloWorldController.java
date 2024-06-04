0c9aae9e772b223cdd84ef9b544583b198ca5f1d
whitecode <guvalakat1603@gmail.com>
2024-06-05 02:39:29 +0500

    /hello-world api is created

--- src/main/java/uz/takhir/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/uz/takhir/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,5 +1,18 @@
 package uz.takhir.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
 //Rest API
+@RestController
 public class HelloWorldController {
+
+    // /hello-world
+
+    //"Hello World"
+    @RequestMapping(method = RequestMethod.GET,path = "/hello-world")
+    public String helloWorld() {
+        return "Hello World!";
+    }
 }
