f877e6a4e994c66bd94ca73146e852073cb1d66e
whitecode <guvalakat1603@gmail.com>
2024-06-09 02:53:26 +0500

    path variables are used

--- src/main/java/uz/takhir/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/uz/takhir/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,9 +1,6 @@
 package uz.takhir.rest.webservices.restfulwebservices.helloworld;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 //Rest API
 @RestController
@@ -21,5 +18,12 @@ public class HelloWorldController {
     @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World!");
+    }
+
+    // Path Parameters
+    // /users/{id}/todos/{id}
+    @GetMapping("/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean("Hello "+name);
     }
 }
