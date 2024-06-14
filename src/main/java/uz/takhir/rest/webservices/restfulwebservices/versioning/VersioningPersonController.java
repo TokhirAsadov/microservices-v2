ec26620ea957a35a745256370d9e533bc1cba2f4
whitecode <guvalakat1603@gmail.com>
2024-06-14 11:29:17 +0500

    VersioningPersonController is created

---
+++ src/main/java/uz/takhir/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -0,0 +1,8 @@
+package uz.takhir.rest.webservices.restfulwebservices.versioning;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersioningPersonController {
+
+}
