a23aca418097a60617a982ff837d4d0ce363d84d
whitecode <guvalakat1603@gmail.com>
2024-06-16 23:38:55 +0500

    /person?version=1 api is created

--- src/main/java/uz/takhir/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ src/main/java/uz/takhir/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -1,6 +1,7 @@
 package uz.takhir.rest.webservices.restfulwebservices.versioning;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,5 +16,10 @@ public class VersioningPersonController {
     @GetMapping("/v2/person")
     public PersonV2 getSecondVersionOfPerson(){
         return new PersonV2(new Name("Tom","Cruse"));
+    }
+
+    @GetMapping(value = "/person",params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParam(){
+        return new PersonV1("Tom Cruse");
     }
 }
