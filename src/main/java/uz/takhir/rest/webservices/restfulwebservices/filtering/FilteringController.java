3c49332496129c2d8b22c03ccd9ae51585d69b08
whitecode <guvalakat1603@gmail.com>
2024-06-19 10:48:07 +0500

    FilteringController is created

---
+++ src/main/java/uz/takhir/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -0,0 +1,7 @@
+package uz.takhir.rest.webservices.restfulwebservices.filtering;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FilteringController {
+}
