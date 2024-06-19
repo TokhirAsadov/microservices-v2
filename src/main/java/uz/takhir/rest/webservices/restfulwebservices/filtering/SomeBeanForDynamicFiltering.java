f38c9ae7f88f7cf7315d2112b82e43dd28e8e62b
whitecode <guvalakat1603@gmail.com>
2024-06-19 11:06:30 +0500

    SomeBeanForDynamicFiltering is created

---
+++ src/main/java/uz/takhir/rest/webservices/restfulwebservices/filtering/SomeBeanForDynamicFiltering.java
@@ -0,0 +1,36 @@
+package uz.takhir.rest.webservices.restfulwebservices.filtering;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+public class SomeBeanForDynamicFiltering {
+    private String field1;
+    private String field2;
+    private String field3;
+
+    public SomeBeanForDynamicFiltering(String field1, String field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public String getField3() {
+        return field3;
+    }
+
+    @Override
+    public String toString() {
+        return "SomeBean{" +
+                "field1='" + field1 + '\'' +
+                ", field2='" + field2 + '\'' +
+                ", field3='" + field3 + '\'' +
+                '}';
+    }
+}
