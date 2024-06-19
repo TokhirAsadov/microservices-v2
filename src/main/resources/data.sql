082c1b1806756cbf3604370d6db6e862ad130217
whitecode <guvalakat1603@gmail.com>
2024-06-19 13:58:15 +0500

    user details are insert into to h2 database

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into user_details(id, birth_date,name)
+values (10001,current_date(),'Tohir');
+
+insert into user_details(id, birth_date,name)
+values (10002,current_date(),'Bahodir');
+
+
+insert into user_details(id, birth_date,name)
+values (10003,current_date(),'Sarvar');
