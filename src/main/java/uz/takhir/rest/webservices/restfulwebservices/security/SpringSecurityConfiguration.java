794cf832b90e593b9257668a2b75432fba1be2ef
Takhir <guvalakat1603@gmail.com>
2024-06-24 23:10:03 +0500

    SpringSecurityConfiguration is created for configuration of security

---
+++ src/main/java/uz/takhir/rest/webservices/restfulwebservices/security/SpringSecurityConfiguration.java
@@ -0,0 +1,7 @@
+package uz.takhir.rest.webservices.restfulwebservices.security;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringSecurityConfiguration {
+}
